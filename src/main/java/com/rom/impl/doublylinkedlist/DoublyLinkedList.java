bbbad84a5a542b9a4b7fe923e3de109f435e70b0
romillimor <romillimor@users.noreply.github.com>
2017-07-25 14:20:25 -0700

    Enhancements to DLL

--- src/main/java/com/rom/impl/doublylinkedlist/DoublyLinkedList.java
+++ src/main/java/com/rom/impl/doublylinkedlist/DoublyLinkedList.java
@@ -1,10 +1,9 @@
 package com.rom.impl.doublylinkedlist;
 
 public class DoublyLinkedList {
-	
+
 	Node head;
-	
-	
+
 	public void add(int data) {
 		Node newNode = new Node(data);
 		Node pointer = head;
@@ -14,8 +13,75 @@ public class DoublyLinkedList {
 			while (pointer.next != null) {
 				pointer = pointer.next;
 			}
-			 newNode.prev = pointer;
-			 pointer.next = newNode;
+			newNode.prev = pointer;
+			pointer.next = newNode;
+		}
+	}
+
+	public void addAtIndex(int index, int data) {
+
+		Node newNode = new Node(data);
+		Node pointer = head;
+		int counter = 0;
+		if (index > this.size() - 1 || index < 0)
+			System.out.println("Invalid Index!");
+		else if (index == 0) {
+			head.prev = newNode;
+			newNode.next = head;
+			head = newNode;
+		} else {
+			while (counter < index) {
+				pointer = pointer.next;
+				counter++;
+			}
+			newNode.prev = pointer.prev;
+			newNode.next = pointer;
+			pointer.prev.next = newNode;
+			pointer.prev = newNode;
+
+		}
+
+	}
+
+	public int size() {
+		Node pointer = head;
+		int count = 1;
+		if (head == null)
+			count = 0;
+		else if (head.next == null)
+			count = 1;
+		else {
+			while (pointer.next != null) {
+				pointer = pointer.next;
+				count++;
+			}
+		}
+		return count;
+
+	}
+
+	public boolean isEmpty() {
+
+		if (head == null)
+			return true;
+		else
+			return false;
+
+	}
+
+	public void delete() {
+		Node pointer = head;
+		if (head == null)
+			System.out.println("List is empty!");
+		else if (head.next == null)
+			head = null;
+		else {
+			while (pointer.next.next != null) {
+				pointer = pointer.next;
+			}
+			pointer.next.prev = null;
+			pointer.next = null;
+
 		}
 	}
 
--- src/main/java/com/rom/impl/doublylinkedlist/ImplDoubly.java
+++ src/main/java/com/rom/impl/doublylinkedlist/ImplDoubly.java
@@ -1,14 +1,18 @@
 package com.rom.impl.doublylinkedlist;
 
 public class ImplDoubly {
-	
+
 	public static void main(String[] args) {
-		
+
 		DoublyLinkedList dList = new DoublyLinkedList();
 		dList.add(56);
 		dList.add(22);
 		dList.add(12);
 		dList.print();
+		dList.addAtIndex(0, 550);
+		dList.print();
+		System.out.println(dList.size() + " " + " " + dList.isEmpty());
+
 	}
-	
+
 }
